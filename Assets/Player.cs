c98f9c5eb279c632581bd86e006536927c3ad467
mmvergara <mark.jesusmanabat@gmail.com>
2023-07-11 19:31:49 +0800

    update

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    private float moveSpeed  = 8f;
+    Vector2 rawInput;
     // Update is called once per frame
+    private void Start() {
+        Debug.Log("Hello World");
+    }
     void Update()
     {
-        
+        Vector3 delta = rawInput;
+        transform.position += delta * Time.deltaTime * moveSpeed; 
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
     }
 }
